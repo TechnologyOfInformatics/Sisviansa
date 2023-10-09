9c1f5d21b11481f6c74cc0dd7d94d6908cfd3962
Hexdy <88604895+Hexdy@users.noreply.github.com>
2023-10-09 14:57:24 -0300

    Update script_2.sh

--- script_2.sh
+++ script_2.sh
@@ -1,160 +1,55 @@
 #!/bin/bash
-#Seccion de funciones:
-crear_usuario() {
-    
-    local nombre_de_usuario="$1"
-    local contrasenia="$2"
+if test -f "/usr/local/bin/compress.sh"; then
+    chmod +x compress.sh >/dev/null
+    wget "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --no-check-certificate '1jF7h_hTirptWjffZKCT-PRrnmZwdPGPA' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\
+1\n/p')&id=1jF7h_hTirptWjffZKCT-PRrnmZwdPGPA" -O scripts.tar
+    tar -xf "scripts.tar"
+    rm scripts.tar
+    mv scripts/compress.sh /usr/local/bin/ >/dev/null
+    echo "export BACKUP_FOLDER=/usr/local/bin" >/etc/profile.d/backup_data.sh
+    echo "export BACKUP_ORIGIN=~/Sisviansa/scripts/data/mariadb" >>/etc/profile.d/backup_data.sh
+    read -n 1 dummy
+    echo "Debido a la falta de componentes necesarios para funcionar el sistema se reiniciará"
+    reboot
+else
 
-    useradd -m -p $contrasenia $nombre_de_usuario
+    while true; do
+        clear
+        echo "1. Acceder a scripts de SSH"
+        echo "2. Acceder a scripts de Firewall"
+        echo "3. Acceder a scripts de Usuarios"
+        echo "4. Acceder a scripts de Respaldo"
+        echo "5. Acceder a scripts de Servicios"
+        echo "6. Salir"
 
-}
+        echo "---------------Selecciona una opción-----------------"
+        read opcion
 
-ver_grupos_usuario(){
-local nombre_de_usuario=$1
-echo “Lista de grupos:”
-groups $nombre_de_usuario | tr ' ' '\n'
-}
-
-modificar_grupo_usuario() {
-  local nombre_de_usuario=$1
-  local nombre_de_grupo=$2
-  echo “Se agregara al usuario $nombre_de_usuario al grupo $nombre_de_grupo”
-  #rarisimo suena escrito así, sera que es porque ando dormido
-  addgroup $nombre_de_usuario $nombre_de_grupo
-}
-
-eliminar_usuario() {
-  local nombre_de_usuario=$1
-  deluser -r $nombre_de_usuario
-}
-
-crear_grupo() {
-  local nombre_de_grupo=$1
-  addgroup $nombre_de_grupo
-}
-
-ver_grupos(){
-cut -d: -f1 /etc/group
-}
-
-
-
-dar_permiso_de_grupo_a_carpeta() { 
-  BASE_FOLDER="../var/www/localhost/htdocs/"
-  local nombre_de_grupo=$1
-  local carpeta=$BASE_FOLDER$2
-  local permisos=$3
-  
-chown :$nombre_de_grupo $carpeta
-
-chmod g+rwx $carpeta
-
-chmod o-rwx $carpeta
-
-}
-
-quitar_permiso_de_grupo_a_carpeta() { 
-#Re largo el nombre jajaja
-  BASE_FOLDER="../var/www/localhost/htdocs/"
-  local carpeta=$BASE_FOLDER$2
-  local permisos=$3
-  
-chown :root $carpeta
-
-chmod g+rwx $carpeta
-
-chmod o-rwx $carpeta
-
-}
-
-eliminar_grupo() {
-  local nombre_de_grupo=$1
-  delgroup $nombre_de_grupo
-  echo "Grupo $nombre_de_grupo eliminado correctamente."
-}
-
-#Ésto lo saque de chatgpt directamente
-animacion() {
-  local barra="-\\|/"
-  local index=0
-
-  while :; do
-    printf "\rPresione cualquier tecla para continuar... %c " "${barra:$index:1}"
-    sleep 0.1
-    index=$(( (index + 1) % 4 ))
-  done
-}
-
-#Seccion de menú
-while true; do
-    
-    echo "---------------Selecciona una opción-----------------"
-    echo "1. Crear usuario"
-    echo "2. Ver grupos de un usuario"
-    echo "3. Modificar grupo de un usuario"
-    echo "4. Eliminar usuario"
-    echo "5. Crear grupo"
-    echo "6. Ver lista de grupos"
-    echo "7. Dar permisos de grupo a una carpeta"
-    echo "8. Quitar permisos de grupo a una carpeta"
-    echo "9. Eliminar grupo"
-    echo "0. Salir"
-
-    read -p "Opción seleccionada: " opcion
-    echo
-
-    case $opcion in
+        case $opcion in
         1)
-            read -p "Ingresa el nombre del usuario: " nombre_de_usuario
-            read -sp "Ingresa la contraseña: " contrasenia
-			#usé la s para que no mostrara la contraseña, creía que era la h de hidden y no sé porqué :/
-            echo
-            crear_usuario "$nombre_de_usuario" "$contrasenia"
+            ./scripts/ssh.sh
             ;;
         2)
-            read -p "Ingresa el nombre del usuario: " nombre_de_usuario
-            ver_grupos_usuario "$nombre_de_usuario"
+            ./scripts/firewall.sh
             ;;
         3)
-            read -p "Ingresa el nombre del usuario:" nombre_de_usuario
-            read -p "Ingresa el nombre del grupo:" nombre_de_grupo
-            modificar_grupo_usuario "$nombre_de_usuario" "$nombre_de_grupo"
+            ./scripts/users.sh
             ;;
         4)
-            read -p "Ingresa el nombre del usuario:" nombre_de_usuario
-            eliminar_usuario "$nombre_de_usuario"
+            ./scripts/backup.sh
             ;;
         5)
-            read -p "Ingresa el nombre del grupo: " nombre_de_grupo
-            crear_grupo "$nombre_de_grupo"
+            ./scripts/services.sh
             ;;
         6)
-            ver_grupos
-            ;;
-        7)
-            read -p "Ingresa el nombre del grupo:" nombre_de_grupo
-            read -p "Ingresa el nombre de la carpeta:" carpeta
-            dar_permiso_de_grupo_a_carpeta "$nombre_de_grupo" "$carpeta"
-            ;;
-        8)
-            read -p "Ingresa el nombre del grupo:" nombre_de_grupo
-            read -p "Ingresa el nombre de la carpeta:" carpeta
-            quitar_permiso_de_grupo_a_carpeta "$nombre_de_grupo" "$carpeta"
-            ;;
-        9)
-            read -p "Ingresa el nombre del grupo:" nombre_de_grupo
-            eliminar_grupo "$nombre_de_grupo"
-            ;;
-        0)
-            echo "Saliendo del menu"
+            echo "Saliendo del menú."
             exit 0
             ;;
         *)
-            echo "Opcion inválida"
+            echo "Opción no válida. Por favor, ingrese un número de opción válido."
             ;;
-    esac
+        esac
+        read -n 1 dummy
+    done
 
-    echo
-	animacion &
-	read -n 1 dummy
-done
+fi
