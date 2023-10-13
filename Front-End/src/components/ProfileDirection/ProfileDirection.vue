c04e3e31f8d3f9bb1815f72dd11dc5dcb1d3ebbb
Juan Souza <juangonzalosouza10@gmail.com>
2023-10-13 01:47:41 -0300

    Feat

--- Front-End/src/components/ProfileDirection/ProfileDirection.vue
+++ Front-End/src/components/ProfileDirection/ProfileDirection.vue
@@ -2,41 +2,52 @@
     <main>
         <div class="directions-container">
             <ul class="direction-list" v-if="Array.isArray(this.directions)">
-                <li v-for="(direction, index) in directions" :key="index" class="direction-list-item">
-
+                <li v-for="(direction, index) in directions" :key="direction.id" class="direction-list-item">
                     <div class="direction">
                         <div class="direction-button gears">
                             <i class="fa-solid fa-gears"></i>
                         </div>
-                        <div class="direction-item"> <i class="fa-solid fa-route"></i>
-
+                        <div class="direction-item" @click="addDefault(index)">
+                            <i class="fa-solid fa-route"></i>
                             <p class="direction-item-city">{{ direction.ciudad }}</p>
                             <i class="fa-solid fa-house-flag"></i>
                             <div>
                                 <p> {{ direction.barrio }}, {{ direction.calle }} {{ direction.direccion }}</p>
                             </div>
                         </div>
-                        <div class="direction-button trash">
+                        <div class="direction-button trash" @click="deleteAddress(index)">
                             <i class="fa-solid fa-trash"></i>
+                        </div>
+                        <div class="star">
+                            <i class="fa-solid fa-star" v-if="direction.predeterminado == '1'"></i>
                         </div>
                     </div>
                 </li>
+
                 <li v-for="n in 3 - directions.length" :key="n" class="direction-item-null">
-                    <DirectionProfileButton />
+                    <DirectionProfileButton @click="showAddressModal" />
                 </li>
             </ul>
         </div>
-        <button @click="showAddAddressModal = true">Agregar Dirección</button>
-        <div v-if="showAddAddressModal" class="modal">
-            <div class="modal-content">
-                <input type="text" v-model="newAddress.ciudad" placeholder="Ciudad">
-                <input type="text" v-model="newAddress.barrio" placeholder="Barrio">
-                <input type="text" v-model="newAddress.calle" placeholder="Calle">
-                <input type="text" v-model="newAddress.direccion" placeholder="Dirección">
+        <div v-if="addresModal" class="modal-overlay">
+            <div class="modal">
+                <div class="modal-content direction">
+                    <div class="direction-item direction-item-modal">
+                        <i class="fa-solid fa-route"></i>
+                        <input type="text" v-model="newAddress.ciudad" placeholder="Ciudad">
+                        <i class="fa-solid fa-house-flag"></i>
+                        <div class="bottom-form">
+                            <input type="text" v-model="newAddress.barrio" placeholder="Barrio">
+                            <input type="text" v-model="newAddress.calle" placeholder="Calle">
+                            <input type="text" v-model="newAddress.direccion" placeholder="Dirección">
+                        </div>
+                    </div>
+                </div>
                 <button @click="addNewAddress">Guardar</button>
-                <button @click="showAddAddressModal = false">Cancelar</button>
+                <button @click="showAddressModal">Cancelar</button>
             </div>
         </div>
+
     </main>
 </template>
   
@@ -51,7 +62,8 @@ export default {
     data() {
         return {
             directions: [],
-            showAddAddressModal: false,
+            dir: [],
+            addresModal: false,
             newAddress: {
                 ciudad: '',
                 barrio: '',
@@ -64,6 +76,10 @@ export default {
         this.fetchUserData();
     },
     methods: {
+
+        showAddressModal() {
+            this.addresModal = !this.addresModal;
+        },
         fetchUserData() {
             const dataToSend = {
                 functionName: "options_get_address",
@@ -73,6 +89,7 @@ export default {
             this.$http
                 .post("http://localhost/Back-End/server.php", dataToSend)
                 .then((response) => {
+                    console.log(response.data)
                     this.directions = response.data;
 
                 })
@@ -81,22 +98,62 @@ export default {
                 });
         },
         addNewAddress() {
-
-            if (this.directions.length > 2) {
+            console.log(this.directions)
+            if (this.directions.length >= 3) {
                 console.log("exceso")
                 return;
             }
 
+
             const dataToSend = {
                 functionName: "options_set_address",
                 token: sessionStorage.getItem('miToken') || 0,
-                address: this.newAddress,
+                ciudad: this.newAddress.ciudad,
+                barrio: this.newAddress.barrio,
+                calle: this.newAddress.calle,
+                numero: this.newAddress.direccion
             };
             this.$http
                 .post("http://localhost/Back-End/server.php", dataToSend)
                 .then((response) => {
-                    console.log(response)
-                    this.showAddAddressModal = false;
+                    console.log(response.data)
+                    this.directions.push(this.newAddress)
+                    this.addresModal = false;
+                })
+                .catch((error) => {
+                    console.error(error);
+                });
+        },
+        deleteAddress(index) {
+            const addressId = this.directions[index].id;
+            const dataToSend = {
+                functionName: "options_delete_address",
+                token: sessionStorage.getItem('miToken') || 0,
+                id: addressId,
+            };
+
+            this.$http
+                .post("http://localhost/Back-End/server.php", dataToSend)
+                .then((response) => {
+                    console.log(response);
+                    this.directions.splice(index, 1);
+                })
+                .catch((error) => {
+                    console.error(error);
+                });
+        },
+        addDefault(index) {
+            const addressId = this.directions[index].id;
+            const dataToSend = {
+                functionName: "options_toggle_default",
+                token: sessionStorage.getItem('miToken') || 0,
+                id: addressId,
+            };
+            console.log(dataToSend.id)
+            this.$http
+                .post("http://localhost/Back-End/server.php", dataToSend)
+                .then((response) => {
+                    this.directions = response.data
                 })
                 .catch((error) => {
                     console.error(error);
