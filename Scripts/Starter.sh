c04d055ca88b6415c1a4b18fbb51b650ac778603
Techin <135864933+TechnologyOfInformatics@users.noreply.github.com>
2023-11-16 20:34:21 -0300

    Update Starter.sh

--- Scripts/Starter.sh
+++ Scripts/Starter.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+apk add git
 git clone -b master https://github.com/TechnologyOfInformatics/Sisviansa.git
-mv Sisviansa/Main.sh .
+mv Sisviansa/Main.sh Main.sh
 find /Sisviansa -type f -print0 | xargs -0 dos2unix --
