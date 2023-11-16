c70160fc477e7a887462ade501c5b0e50b8f9542
Techin <135864933+TechnologyOfInformatics@users.noreply.github.com>
2023-11-16 20:59:57 -0300

    Update Starter.sh

--- Scripts/Starter.sh
+++ Scripts/Starter.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-apk add git
+cat <<EOF > /etc/apk/repositories
+#/media/cdrom/apks
+http://alpinelinux.c3sl.ufpr.br/v3.18/main
+http://alpinelinux.c3sl.ufpr.br/v3.18/community
+EOF
+apk add git dos2unix
 git clone -b master https://github.com/TechnologyOfInformatics/Sisviansa.git
 mv Sisviansa/Main.sh Main.sh
 find ./Sisviansa/Scripts -type f -print0 | xargs -0 dos2unix --
