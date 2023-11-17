11ea7c7cb6bf288980e6b44adba5adbcc77ce649
Hexdy <88604895+Hexdy@users.noreply.github.com>
2023-11-17 06:40:50 -0300

    Update Starter.sh

--- Scripts/Starter.sh
+++ Scripts/Starter.sh
@@ -18,7 +18,7 @@ git clone -b master https://github.com/TechnologyOfInformatics/Sisviansa.git
 mv Sisviansa/Main.sh Main.sh >/dev/null
 find ./Sisviansa/Scripts -type f -print0 | xargs -0 dos2unix --
 dos2unix Main.sh
-find ./Sisviansa/Data -type f -not -path '*/\.*' -exec sed -i 's/sisviansa_mariadb/'"$(ip a show eth0 | grep -oE 'inet ([0-9]{1,3}\.){3}[0-9]{1,3}' | cut -d ' ' -f 2)"'/g' {} + ;
+#find ./Sisviansa/Data -type f -not -path '*/\.*' -exec sed -i 's/sisviansa_mariadb/'"$(ip a show eth0 | grep -oE 'inet ([0-9]{1,3}\.){3}[0-9]{1,3}' | cut -d ' ' -f 2)"'/g' {} + ;
 find ./Sisviansa/Data -type f -not -path '*/\.*' -exec sed -i 's/localhost/'"$(ip a show eth0 | grep -oE 'inet ([0-9]{1,3}\.){3}[0-9]{1,3}' | cut -d ' ' -f 2)"'/g' {} + ;
 find ./Sisviansa/Scripts -type f -exec chmod +x {} \;
 
